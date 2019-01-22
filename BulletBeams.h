0bb42e51a19afacd51c5697ad215e76da209d2b4
linescc <46932740+linescc@users.noreply.github.com>
2019-01-22 11:51:55 -0400

    Add files via upload

---
+++ BulletBeams.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <vector>
+struct BulletImpactInfo
+{
+	float m_flExpTime;
+	Vector m_vecHitPos;
+};
+class BulletBeamsEvent : public IGameEventListener2, public Singleton<BulletBeamsEvent>
+{
+public:
+	void omfg();
+	void comment();
+	void FireGameEvent(IGameEvent *event);
+	int  GetEventDebugID(void);
+	void RegisterSelf();
+	void UnregisterSelf();
+	void Paint(void);
+private:
+	std::vector<BulletImpactInfo> bulletImpactInfo;
+};
---
+++ backtrack.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "../Sp1Ky Private/features/MainInclude.hpp"
+#define NUM_OF_TICKS 12
+class QAngle;
+struct StoredData
+{
+	float simtime;
+	Vector hitboxPos;
+	Vector origin;
+	QAngle viewangle;
+	Vector headpos;
+};
+class TimeWarp : public Singleton<TimeWarp>
+{
+	int nLatestTick;
+public:
+	StoredData TimeWarpData[64][NUM_OF_TICKS];
+	void lmfaohaha();
+	void cleanasfu();
+	void CreateMove(CUserCmd* userCMD);
+	void pleaseneverfall();
+	void loveme();
+};
